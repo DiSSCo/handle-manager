65b36ab1325ec26c866e01c653baf8a629d42a87
southeo <soulaine.theo@gmail.com>
2022-06-29 19:35:03 +0200

    Introducing openFeign client. Security and encryption capabilities added

--- handle-manager/src/main/java/com/example/handlemanager/client/HandleClient.java
+++ handle-manager/src/main/java/com/example/handlemanager/client/HandleClient.java
@@ -1,7 +1,27 @@
 package com.example.handlemanager.client;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
-@FeignClient
-public interface HandleClient {
-
+@FeignClient(url = "https://35.178.174.137:8000")
+public interface HandleClient{
+	
+	// Create new handle record (Note: Handle API uses PUT for new handles and updating handles)
+	@PutMapping(value = "/api/handles/") 
+	String createHandleRecord();
+	
+	// Resolve handle
+	@GetMapping(value="/api/handles/{handle}")
+	String resolveHandle(@PathVariable("handle") String handle);
+	
+	// Update existing handle record
+	@PutMapping(value = "/api/handles/{handle}") 
+	String updateHandleRecord(@PathVariable("handle") String handle);
+	
+	@DeleteMapping(value="/api/handles/{handle}")
+	String deleteHandle(@PathVariable("handle") String handle);
+	
 }
+
--- handle-manager/src/test/java/com/example/handlemanager/HandleManagerScratchApplicationTests.java
+++ handle-manager/src/test/java/com/example/handlemanager/HandleManagerScratchApplicationTests.java
@@ -7,11 +7,13 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.security.PrivateKey;
 import java.time.Instant;
+import java.util.Base64;
 
 import org.junit.jupiter.api.Test;
 
 import com.example.handlemanager.model.*;
 import com.example.handlemanager.repository.HandleObjectRepository;
+import com.example.handlemanager.security.HandleSessionsAuthenticator;
 import com.example.handlemanager.service.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -31,7 +33,6 @@ class HandleManagerScratchApplicationTests {
 	HandleService service = new HandleService();
 	PrivateKey privkey;
 	
-	@Test
 	void testRecordStr() {
 		DigitalSpecimen sample = DigitalSpecimen.testDigitalSpecimen();
 		String now = Instant.ofEpochSecond(System.currentTimeMillis()).toString();
@@ -39,73 +40,18 @@ class HandleManagerScratchApplicationTests {
 	}
 	
 	
-	/*
-	void testToDS() throws JsonMappingException, JsonProcessingException, HandleException{
+	@Test void testNonce() throws IOException, GeneralSecurityException {
+		HandleSessionsAuthenticator auth  = new HandleSessionsAuthenticator(null, null);
+		byte[] a = auth.generateCnonceBytes();
 		
-		//HandleObjectRepositoryWeb rep = new HandleObjectRepositoryWeb();
+		String nonceStr = auth.encodeToString(a);
+		HandleSessionsAuthenticator auth2  = new HandleSessionsAuthenticator(null, nonceStr);
+		String authStr = auth2.createAuthHeader();
 		
-		String handle = "20.5000.1025/0DJS-01NM";
-		byte[][] types = null;
-		int[] idxs = null;
+		System.out.println(authStr);
 		
-		//System.out.println(rep.resolveHandle(handle, types, idxs));
-	}
-	
-	void testDigSpecIngestion() {
-		
-		DigitalSpecimen testDS = DigitalSpecimen.testDigitalSpecimen();
-		System.out.println(testDS.toString());	
-		HandleValue[] testHV = testDS.toHandleValueList();
 
-		for (int i=0;i<testHV.length;i++) {
-			System.out.println(testHV[i].getTypeAsString() + " : " + testHV[i].getDataAsString());
-		}
-		
 	}
-	
-	void testTypeBytes() {
-		String[] typeList = {"URL",
-			"OBJECT_TYPE",
-			"CURATED_OBJECT_ID"};
-		service.typeBytes(typeList);
-		
-	}
-	
-	@Test
-	void testAdminRejection() throws HandleException {
-		String[] typeList = {"URL",
-				"OBJECT_TYPE",
-				"CURATED_OBJECT_ID"};
-		
-		HandleValue values[] = new HandleResolver().resolveHandle("20.5000.1025/YVQX-9P0G", null, null);
-		for (int i=0; i < values.length; i++){
-			System.out.println(String.valueOf(values[i]));
-		}
-	}
-	
-	void testKey() {
-		String filename = "PrivateKey/Location";
-		PrivateKey pk = null;
-		String alg = "SHA-256";
-		File file = new File(filename);
-		if (!file.exists()) {
-			System.out.println("Private Key file not found: "+filename);
-		}
-		try(DataInputStream stream = new DataInputStream(new FileInputStream(file))){
-			//pk = PrivateKeyReader.getPrivatekey(stream, alg);
-		}
-		catch(IOException e) {
-			System.out.println("Failed to load private key from file "+filename );
-		}
-		
-	}*/
-
-		
-		
-			
-//		return retStr;
-		//HandleValue values[] = new HandleResolver().resolveHandle(handle, null, null);
-		
 } 
 	
 
