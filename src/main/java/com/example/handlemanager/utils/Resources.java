bfbbe084ca1959f92fab2b397db35f499101a4e5
southeo <soulaine.theo@gmail.com>
2022-11-14 13:38:20 +0100

    Refactor xml formatting

--- src/main/java/com/example/handlemanager/utils/Resources.java
+++ src/main/java/com/example/handlemanager/utils/Resources.java
@@ -1,8 +1,20 @@
 package com.example.handlemanager.utils;
 
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import com.example.handlemanager.model.repositoryObjects.Handles;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 public class Resources {
 
@@ -14,7 +26,6 @@ public class Resources {
 		}
 		return ""; // This should maybe return a warning?
 	}
-
 	public static Handles genAdminHandle(byte[] handle, long timestamp) {
 		return new Handles(handle, 100, "HS_ADMIN".getBytes(), decodeAdmin(), timestamp);
 	}
@@ -37,6 +48,33 @@ public class Resources {
 	private static int toDigit(char hexChar) {
 		int digit = Character.digit(hexChar, 16);
 		return digit;
+	}
+
+	public static byte[] setLocations(String[] objectLocations) throws TransformerException, ParserConfigurationException {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = dbf.newDocumentBuilder();
+
+		var doc = documentBuilder.newDocument();
+		var locations = doc.createElement("locations");
+		doc.appendChild(locations);
+		for (int i = 0; i < objectLocations.length; i++) {
+
+			var locs = doc.createElement("location");
+			locs.setAttribute("id", String.valueOf(i));
+			locs.setAttribute("href", objectLocations[i]);
+			locs.setAttribute("weight", "0");
+			locations.appendChild(locs);
+		}
+		return documentToString(doc).getBytes(StandardCharsets.UTF_8);
+	}
+
+	private static String documentToString(Document document) throws TransformerException {
+		var tf = TransformerFactory.newInstance();
+		var transformer = tf.newTransformer();
+		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+		StringWriter writer = new StringWriter();
+		transformer.transform(new DOMSource(document), new StreamResult(writer));
+		return writer.getBuffer().toString();
 	}
 
 }
