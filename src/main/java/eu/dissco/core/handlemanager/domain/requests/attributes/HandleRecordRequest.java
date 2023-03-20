4515731f37a6fc10e8839861ba0adbe66b0f6702
southeo <soulaine.theo@gmail.com>
2023-03-20 12:01:42 +0100

    Locations attribute is optional in request

--- src/main/java/eu/dissco/core/handlemanager/domain/requests/attributes/HandleRecordRequest.java
+++ src/main/java/eu/dissco/core/handlemanager/domain/requests/attributes/HandleRecordRequest.java
@@ -2,7 +2,6 @@ package eu.dissco.core.handlemanager.domain.requests.attributes;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
-import javax.validation.constraints.NotEmpty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -23,9 +22,7 @@ public class HandleRecordRequest {
   @JsonProperty(required = true)
   @JsonPropertyDescription("PID for the Subtype of Digital Object.")
   private final String digitalObjectSubtypePid;
-  @JsonProperty(required = true)
   @JsonPropertyDescription("Array containing the locations of the object.")
-  @NotEmpty
   private final String[] locations;
 
 }
