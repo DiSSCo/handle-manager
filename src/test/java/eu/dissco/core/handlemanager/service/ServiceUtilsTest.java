b470cb33883cd6ab121ef049ceb918ee973a1831
southeo <soulaine.theo@gmail.com>
2023-04-03 11:26:15 +0200

    TestCoverage

--- src/test/java/eu/dissco/core/handlemanager/service/ServiceUtilsTest.java
+++ src/test/java/eu/dissco/core/handlemanager/service/ServiceUtilsTest.java
@@ -6,15 +6,19 @@ import static eu.dissco.core.handlemanager.testUtils.TestUtils.DIGITAL_OBJECT_TY
 import static eu.dissco.core.handlemanager.testUtils.TestUtils.DIGITAL_OR_PHYSICAL_TESTVAL;
 import static eu.dissco.core.handlemanager.testUtils.TestUtils.IN_COLLECTION_FACILITY_TESTVAL;
 import static eu.dissco.core.handlemanager.testUtils.TestUtils.LOC_TESTVAL;
+import static eu.dissco.core.handlemanager.testUtils.TestUtils.MEDIA_HASH_TESTVAL;
+import static eu.dissco.core.handlemanager.testUtils.TestUtils.MEDIA_URL_TESTVAL;
 import static eu.dissco.core.handlemanager.testUtils.TestUtils.PHYSICAL_IDENTIFIER_CETAF;
 import static eu.dissco.core.handlemanager.testUtils.TestUtils.PID_ISSUER_PID;
 import static eu.dissco.core.handlemanager.testUtils.TestUtils.PREFIX;
 import static eu.dissco.core.handlemanager.testUtils.TestUtils.REFERENT_DOI_NAME_PID;
 import static eu.dissco.core.handlemanager.testUtils.TestUtils.SPECIMEN_HOST_PID;
 import static eu.dissco.core.handlemanager.testUtils.TestUtils.genDigitalSpecimenRequestObject;
+import static eu.dissco.core.handlemanager.testUtils.TestUtils.genMediaRequestObject;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import eu.dissco.core.handlemanager.domain.requests.attributes.DigitalSpecimenRequest;
+import eu.dissco.core.handlemanager.domain.requests.attributes.MediaObjectRequest;
 import eu.dissco.core.handlemanager.domain.requests.attributes.PhysicalIdType;
 import eu.dissco.core.handlemanager.domain.requests.attributes.PhysicalIdentifier;
 import eu.dissco.core.handlemanager.testUtils.TestUtils;
@@ -49,6 +53,33 @@ import org.junit.jupiter.api.Test;
    assertThat(result).isEqualTo(expected);
   }
 
+  @Test
+  void testSetUniquePhysicalIdentifierMedia(){
+   // Given
+   var request = genMediaRequestObject();
+   var expected = request.getSubjectPhysicalIdentifier().physicalId();
+
+   // When
+   var result =new String(setUniquePhysicalIdentifierId(request), StandardCharsets.UTF_8);
+
+   // Then
+   assertThat(result).isEqualTo(expected);
+  }
+
+  @Test
+  void testSetUniquePhysicalIdentifierMediaCombined(){
+   // Given
+   var request = givenCombinedMediaRequest();
+   var suffix = request.getSubjectSpecimenHostPid().replace(PREFIX + "/","");
+   var expected = request.getSubjectPhysicalIdentifier().physicalId() + ":" + suffix;
+
+   // When
+   var result = new String(setUniquePhysicalIdentifierId(request), StandardCharsets.UTF_8);
+
+   // Then
+   assertThat(result).isEqualTo(expected);
+  }
+
   private DigitalSpecimenRequest givenCombinedTypeDSRecord(){
    return new DigitalSpecimenRequest(
        PID_ISSUER_PID,
@@ -59,6 +90,20 @@ import org.junit.jupiter.api.Test;
        DIGITAL_OR_PHYSICAL_TESTVAL,
        SPECIMEN_HOST_PID,
        IN_COLLECTION_FACILITY_TESTVAL,
+       new PhysicalIdentifier("id", PhysicalIdType.COMBINED)
+   );
+  }
+
+  private MediaObjectRequest givenCombinedMediaRequest(){
+   return new MediaObjectRequest(
+       PID_ISSUER_PID,
+       DIGITAL_OBJECT_TYPE_PID,
+       DIGITAL_OBJECT_SUBTYPE_PID,
+       LOC_TESTVAL,
+       REFERENT_DOI_NAME_PID,
+       MEDIA_HASH_TESTVAL,
+       MEDIA_URL_TESTVAL,
+       SPECIMEN_HOST_PID,
        new PhysicalIdentifier("id", PhysicalIdType.COMBINED)
    );
   }
