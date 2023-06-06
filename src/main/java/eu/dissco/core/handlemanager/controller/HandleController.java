4e9c826a9f3b4730c9962544d257f11ee1a4c791
southeo <soulaine.theo@gmail.com>
2023-06-06 13:46:01 +0200

    Kubernetes env (#21) (#24)
    * Kubernetes env
    
    * Remove keycloak

--- src/main/java/eu/dissco/core/handlemanager/controller/HandleController.java
+++ src/main/java/eu/dissco/core/handlemanager/controller/HandleController.java
@@ -109,6 +109,7 @@ public class HandleController {
   @PostMapping(value = "/batch")
   public ResponseEntity<JsonApiWrapperWrite> createRecords(@RequestBody List<JsonNode> requests)
       throws PidResolutionException, PidServiceInternalError, InvalidRequestException, PidCreationException {
+    log.info("received batch request");
 
     for (JsonNode request : requests) {
       schemaValidator.validatePostRequest(request);
