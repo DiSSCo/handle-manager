9c16564ce49fc7d16c91572b573dad7c955c32ad
southeo <soulaine.theo@gmail.com>
2023-10-03 10:37:31 +0200

    test fix

--- src/test/java/eu/dissco/core/handlemanager/service/PidServiceTest.java
+++ src/test/java/eu/dissco/core/handlemanager/service/PidServiceTest.java
@@ -22,7 +22,7 @@ import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-class PidServiceTest {
+abstract class PidServiceTest {
 
   @Mock
   protected HandleRepository handleRep;
