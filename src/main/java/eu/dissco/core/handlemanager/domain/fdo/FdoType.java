c74039bf6a39c0566247bb6dd5ccdaf2bdf6852b
southeo <soulaine.theo@gmail.com>
2024-08-16 10:12:12 +0200

    update json schemas

--- src/main/java/eu/dissco/core/handlemanager/domain/fdo/FdoType.java
+++ src/main/java/eu/dissco/core/handlemanager/domain/fdo/FdoType.java
@@ -10,55 +10,55 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Getter
 public enum FdoType {
-  @JsonProperty("https://hdl.handle.net/21.T11148/532ce6796e2828dd2be6") HANDLE(
+  @JsonProperty("https://doi.org/21.T11148/532ce6796e2828dd2be6") HANDLE(
       "Handle Kernel",
-      "https://hdl.handle.net/21.T11148/532ce6796e2828dd2be6",
-      "https://hdl.handle.net/21.T11148/532ce6796e2828dd2be6",
+      "https://doi.org/21.T11148/532ce6796e2828dd2be6",
+      "https://doi.org/21.T11148/532ce6796e2828dd2be6",
       HANDLE_DOMAIN),
-  @JsonProperty("https://hdl.handle.net/21.T11148/527856fd709ec8c5bc8c") DOI(
+  @JsonProperty("https://doi.org/21.T11148/527856fd709ec8c5bc8c") DOI(
       "DOI Kernel",
-      "https://hdl.handle.net/21.T11148/527856fd709ec8c5bc8c",
-      "https://hdl.handle.net/21.T11148/527856fd709ec8c5bc8c",
+      "https://doi.org/21.T11148/527856fd709ec8c5bc8c",
+      "https://doi.org/21.T11148/527856fd709ec8c5bc8c",
       DOI_DOMAIN),
-  @JsonProperty("https://hdl.handle.net/21.T11148/894b1e6cad57e921764e") DIGITAL_SPECIMEN(
+  @JsonProperty("https://doi.org/21.T11148/894b1e6cad57e921764e") DIGITAL_SPECIMEN(
       "DigitalSpecimen",
-      "https://hdl.handle.net/21.T11148/894b1e6cad57e921764e",
-      "https://hdl.handle.net/21.T11148/894b1e6cad57e921764e",
+      "https://doi.org/21.T11148/894b1e6cad57e921764e",
+      "https://doi.org/21.T11148/894b1e6cad57e921764e",
       DOI_DOMAIN),
-  @JsonProperty("https://hdl.handle.net/21.T11148/bbad8c4e101e8af01115") DIGITAL_MEDIA(
+  @JsonProperty("https://doi.org/21.T11148/bbad8c4e101e8af01115") DIGITAL_MEDIA(
       "MediaObject",
-      "https://hdl.handle.net/21.T11148/bbad8c4e101e8af01115",
-      "https://hdl.handle.net/21.T11148/bbad8c4e101e8af01115",
+      "https://doi.org/21.T11148/bbad8c4e101e8af01115",
+      "https://doi.org/21.T11148/bbad8c4e101e8af01115",
       DOI_DOMAIN),
-  @JsonProperty("https://hdl.handle.net/21.T11148/cf458ca9ee1d44a5608f") ANNOTATION(
+  @JsonProperty("https://doi.org/21.T11148/cf458ca9ee1d44a5608f") ANNOTATION(
       "Annotation",
-      "https://hdl.handle.net/21.T11148/cf458ca9ee1d44a5608f",
-      "https://hdl.handle.net/21.T11148/cf458ca9ee1d44a5608f",
+      "https://doi.org/21.T11148/cf458ca9ee1d44a5608f",
+      "https://doi.org/21.T11148/cf458ca9ee1d44a5608f",
       HANDLE_DOMAIN),
-  @JsonProperty("https://hdl.handle.net/21.T11148/417a4f472f60f7974c12") SOURCE_SYSTEM(
+  @JsonProperty("https://doi.org/21.T11148/417a4f472f60f7974c12") SOURCE_SYSTEM(
       "sourceSystem",
-      "https://hdl.handle.net/21.T11148/417a4f472f60f7974c12",
-      "https://hdl.handle.net/21.T11148/417a4f472f60f7974c12",
+      "https://doi.org/21.T11148/417a4f472f60f7974c12",
+      "https://doi.org/21.T11148/417a4f472f60f7974c12",
       HANDLE_DOMAIN),
-  @JsonProperty("https://hdl.handle.net/21.T11148/ce794a6f4df42eb7e77e") DATA_MAPPING(
+  @JsonProperty("https://doi.org/21.T11148/ce794a6f4df42eb7e77e") DATA_MAPPING(
       "Mapping",
-      "https://hdl.handle.net/21.T11148/ce794a6f4df42eb7e77e",
-      "https://hdl.handle.net/21.T11148/ce794a6f4df42eb7e77e",
+      "https://doi.org/21.T11148/ce794a6f4df42eb7e77e",
+      "https://doi.org/21.T11148/ce794a6f4df42eb7e77e",
       HANDLE_DOMAIN),
-  @JsonProperty("https://hdl.handle.net/21.T11148/413c00cbd83ae33d1ac0") ORGANISATION(
+  @JsonProperty("https://doi.org/21.T11148/413c00cbd83ae33d1ac0") ORGANISATION(
       "Organisation",
-      "https://hdl.handle.net/21.T11148/413c00cbd83ae33d1ac0",
-      "https://hdl.handle.net/21.T11148/413c00cbd83ae33d1ac0",
+      "https://doi.org/21.T11148/413c00cbd83ae33d1ac0",
+      "https://doi.org/21.T11148/413c00cbd83ae33d1ac0",
       HANDLE_DOMAIN),
-  @JsonProperty("https://hdl.handle.net/21.T11148/d7570227982f70256af3") TOMBSTONE(
+  @JsonProperty("https://doi.org/21.T11148/d7570227982f70256af3") TOMBSTONE(
       "Tombstone",
-      "https://hdl.handle.net/21.T11148/d7570227982f70256af3",
-      "https://hdl.handle.net/21.T11148/d7570227982f70256af3",
+      "https://doi.org/21.T11148/d7570227982f70256af3",
+      "https://doi.org/21.T11148/d7570227982f70256af3",
       HANDLE_DOMAIN),
-  @JsonProperty("https://hdl.handle.net/21.T11148/22e71a0015cbcfba8ffa") MAS(
+  @JsonProperty("https://doi.org/21.T11148/22e71a0015cbcfba8ffa") MAS(
       "Machine Annotation Service",
-      "https://hdl.handle.net/21.T11148/22e71a0015cbcfba8ffa",
-      "https://hdl.handle.net/21.T11148/22e71a0015cbcfba8ffa",
+      "https://doi.org/21.T11148/22e71a0015cbcfba8ffa",
+      "https://doi.org/21.T11148/22e71a0015cbcfba8ffa",
       HANDLE_DOMAIN);
 
   private final String digitalObjectName;
