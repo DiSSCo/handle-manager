770310b29565272a722c930a19380f7eb5b80555
Sam Leeflang <sam.leeflang@naturalis.nl>
2025-07-14 11:24:10 +0200

    Code review

--- src/main/java/eu/dissco/core/handlemanager/domain/openapi/patch/VirtualCollectionPatchRequest.java
+++ src/main/java/eu/dissco/core/handlemanager/domain/openapi/patch/VirtualCollectionPatchRequest.java
@@ -1,7 +1,7 @@
 package eu.dissco.core.handlemanager.domain.openapi.patch;
 
 import eu.dissco.core.handlemanager.domain.fdo.FdoType;
-import eu.dissco.core.handlemanager.schema.SourceSystemRequestAttributes;
+import eu.dissco.core.handlemanager.schema.VirtualCollectionRequestAttributes;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(name = "Virtual Collection patch request", description = "Virtual Collection schema - Handle mode only")
@@ -12,6 +12,6 @@ public record VirtualCollectionPatchRequest(
         "https://hdl.handle.net/21.T11148/2ac65a933b7a0361b651"
     })
     FdoType type,
-    SourceSystemRequestAttributes attributes) {
+    VirtualCollectionRequestAttributes attributes) {
 
 }
--- src/main/java/eu/dissco/core/handlemanager/domain/openapi/post/VirtualCollectionPostRequest.java
+++ src/main/java/eu/dissco/core/handlemanager/domain/openapi/post/VirtualCollectionPostRequest.java
@@ -1,7 +1,7 @@
 package eu.dissco.core.handlemanager.domain.openapi.post;
 
 import eu.dissco.core.handlemanager.domain.fdo.FdoType;
-import eu.dissco.core.handlemanager.schema.SourceSystemRequestAttributes;
+import eu.dissco.core.handlemanager.schema.VirtualCollectionRequestAttributes;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(name = "Virtual Collection post request", description = "Virtual Collection schema - Handle mode only")
@@ -11,5 +11,6 @@ public record VirtualCollectionPostRequest(
         "https://hdl.handle.net/21.T11148/2ac65a933b7a0361b651"
     })
     FdoType type,
-    SourceSystemRequestAttributes attributes) {
+    VirtualCollectionRequestAttributes attributes) {
+
 }
